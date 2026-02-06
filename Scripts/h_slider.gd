c406d219d450538b997a05554e769a4ee679a55b
MahderK <mahderkassaw@gmail.com>
2026-02-06 17:03:10 +0300

    Added options script Modified h_slider script

--- Scripts/h_slider.gd
+++ Scripts/h_slider.gd
@@ -1,11 +1,19 @@
 extends HSlider
 
+@export var bus_name: String
 
-# Called when the node enters the scene tree for the first time.
+var bus_index: int
+
 func _ready() -> void:
-	pass # Replace with function body.
+	bus_index = AudioServer.get_bus_index(bus_name)
+	value_changed.connect(_on_value_changed)
 
+	value = db_to_linear(
+		AudioServer.get_bus_volume_db(bus_index)
+	)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(
+		bus_index,
+		linear_to_db(value)
+	)
