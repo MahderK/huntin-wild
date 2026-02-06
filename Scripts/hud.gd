cef1d2817ac40865c60c1c8dd4975a6a7c2e35ed
MahderK <mahderkassaw@gmail.com>
2026-02-06 19:31:13 +0300

    Added sun_pickup asset created hud.gd created game.gd

---
+++ Scripts/hud.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+@onready var label = $Clock
+@onready var timer = $Timer
+@onready var tot_time_in_sec : int = 300
+
+signal time_change(total_seconds)
+
+func _ready():
+	$Timer.start()
+
+
+
+func _on_timer_timeout() -> void:
+	tot_time_in_sec += 1
+	var hour = int(tot_time_in_sec/ 50)
+	var minute = (tot_time_in_sec % 50) * 1.2
+
+	$Clock.text = '%02d: %02d' % [hour, minute]
+
+	time_change.emit(tot_time_in_sec)
+
+	if tot_time_in_sec >= 1199:
+		tot_time_in_sec = 300
