f0c53e093b09f3d4aece0a68a6f5b60e4699509a
MahderK <mahderkassaw@gmail.com>
2026-02-07 10:23:44 +0300

    Finished day-night cycle

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,11 +1,9 @@
 extends Node2D
 
+@export var day_night_gradient: Gradient
+@onready var modulate_node = $CanvasModulate
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_hud_time_change(total_seconds:Variant) -> void:
+	var progress = float(total_seconds) / 1000.0
+	var final_color = day_night_gradient.sample(progress)
+	modulate_node.color = final_color
--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -13,7 +13,7 @@ func _ready():
 
 func _on_timer_timeout() -> void:
 	tot_time_in_sec += 1
-	var hour = int(tot_time_in_sec/ 50)
+	var hour = float(tot_time_in_sec/ 50)
 	var minute = (tot_time_in_sec % 50) * 1.2
 
 	$Clock.text = '%02d: %02d' % [hour, minute]
@@ -21,4 +21,4 @@ func _on_timer_timeout() -> void:
 	time_change.emit(tot_time_in_sec)
 
 	if tot_time_in_sec >= 1199:
-		tot_time_in_sec = 300
+		tot_time_in_sec = 0
